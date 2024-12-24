f2c5c71e72d93bccda677c600b3cbceb8a09c80f
Antix/Switch <65674449+antixcodes@users.noreply.github.com>
2024-12-24 23:21:45 +0530

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -33,7 +33,7 @@ class general(commands.Cog):
     async def staffreq(self, ctx):
             await ctx.message.delete() 
             embed = nextcord.Embed(title="Galaxy Exchange Staff Req", color=rang)
-            embed.add_field(name="<:alert:1321168458016358448> Req:", value=f"<:arrow_downright:1305955462700863498> 30+ vouches = 2$ Exchange/mm limit\n<:arrow_downright:1305955462700863498> 50+ vouches = 4$ Exchange/mm limit\n**TOS: Must have to add our vanity in your status/about**", inline=False)
+            embed.add_field(name="<:alert:1321168458016358448> Req:", value=f"<:arrow_downright:1305955462700863498> 30+ vouches = 2$ Exchange/mm limit\n<:arrow_downright:1305955462700863498> 50+ vouches = 4$ Exchange/mm limit\n**TOS: Must have to add our vanity in your status/about\nYou must agree to exchange at our exchange rate > <#1320005089343307868>**", inline=False)
             embed.add_field(name="Want more exchange/mm limit", value=f"> Deposite Security Amount Above your Limit\n> Eg: Your limit is 2$ and you want limit of 4$ then deposit 2$ as security", inline=False)
             embed.set_footer(text=f"Galaxy Exchanges | MM | Exchanges")
             embed.set_thumbnail(url=ctx.guild.icon.url)
